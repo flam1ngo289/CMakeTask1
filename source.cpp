4f0f06ac879cfbed3e7c46528c04b7262ac8a85d
flam1ngo <kris.s.03@mail.ru>
2024-03-20 22:32:55 +0300

    Initial commit

---
+++ source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!";
+}
